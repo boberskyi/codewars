5fe93dd3d2abc16ca6e37dad3814ae1782abee5d
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2022-11-05 14:02:20 +0100

    quick fix

--- Find the first non-consecutive number.js
+++ Find the first non-consecutive number.js
@@ -1,5 +1,5 @@
 const firstNonConsecutive = arr => {
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length - 1; i++) {
     if (arr[i] + 1 !== arr[i + 1]) {
       return arr[i + 1];
     }
