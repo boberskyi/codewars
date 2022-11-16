d9e7b4328e78af327e8493464b44bacd597d53d4
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2022-11-16 12:05:04 +0100

    Expressions Matter

---
+++ Expressions Matter.js
@@ -0,0 +1,10 @@
+const expressionMatter = (a, b, c) => {
+  const results = [];
+  results.push(a + b + c);
+  results.push(a * b * c);
+  results.push(a + b * c);
+  results.push((a + b) * c);
+  results.push(a * b + c);
+  results.push(a * (b + c));
+  return Math.max(...results);
+}
