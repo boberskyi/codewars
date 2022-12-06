fe040112bf0e82b5d27311b7d043762676d8b435
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2022-12-06 07:54:41 +0100

    Create Find the odd int.js

---
+++ Find the odd int.js
@@ -0,0 +1,11 @@
+const findOdd = A => {
+  const result = A.reduce((acc, item) => {
+    if (acc[item]) {
+      acc[item].push(item);
+    } else {
+      acc[item] = [item];
+    }
+    return acc;
+  }, {});
+  return Object.values(result).filter(item => item.length % 2 !== 0)[0][0];
+}
