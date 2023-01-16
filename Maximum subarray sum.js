0b05bb5a997329f72595e8197557567420f164dd
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-01-16 07:53:26 +0100

    Update Maximum subarray sum.js

--- Maximum subarray sum.js
+++ Maximum subarray sum.js
@@ -1,6 +1,6 @@
-var maxSequence = function (arr) {
-  var currentSum = 0;
-  return arr.reduce(function (maxSum, number) {
+const maxSequence = arr => {
+  let currentSum = 0;
+  return arr.reduce((maxSum, number) => {
     currentSum = Math.max(currentSum + number, 0);
     return Math.max(currentSum, maxSum);
   }, 0);
