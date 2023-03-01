24967698061ca486aa8a92663a6cdc29c68348d1
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-03-01 06:55:38 +0100

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,21 @@
+const greet = lang => langs[lang] || langs['english'];
+
+var langs = {
+  'english': 'Welcome',
+  'czech': 'Vitejte',
+  'danish': 'Velkomst',
+  'dutch': 'Welkom',
+  'estonian': 'Tere tulemast',
+  'finnish': 'Tervetuloa',
+  'flemish': 'Welgekomen',
+  'french': 'Bienvenue',
+  'german': 'Willkommen',
+  'irish': 'Failte',
+  'italian': 'Benvenuto',
+  'latvian': 'Gaidits',
+  'lithuanian': 'Laukiamas',
+  'polish': 'Witamy',
+  'spanish': 'Bienvenido',
+  'swedish': 'Valkommen',
+  'welsh': 'Croeso'
+};
