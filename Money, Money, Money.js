6677d01ff1efd66c5f2cec850c8b2ccdd26a4bac
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-03-09 20:27:20 +0100

    Create Money, Money, Money.js

---
+++ Money, Money, Money.js
@@ -0,0 +1,9 @@
+const calculateYears = (principal, interest, tax, desired) => {
+  let years = 0;
+
+  while (principal < desired) {
+    principal += principal * interest * (1 - tax);
+    years++;
+  }
+  return years;
+}
