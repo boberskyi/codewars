8e6817dfda402d883cb969c915d81a735723d4da
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-07-10 06:48:03 +0200

    Create Create Phone Number.js

---
+++ Create Phone Number.js
@@ -0,0 +1,9 @@
+const createPhoneNumber = numbers => {
+  var format = "(xxx) xxx-xxxx";
+
+  for (let i = 0; i < numbers.length; i++) {
+    format = format.replace('x', numbers[i]);
+  }
+
+  return format;
+}
