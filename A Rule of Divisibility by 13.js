ec6268b2137495122656b7da39de88a00832e165
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-07-18 09:54:51 +0200

    Create A Rule of Divisibility by 13.js

---
+++ A Rule of Divisibility by 13.js
@@ -0,0 +1,5 @@
+const thirt = n => {
+  const nums = [1, 10, 9, 12, 3, 4];
+  var sum = ('' + n).split('').reverse().reduce((sum, v, i) => sum + v * nums[i % nums.length], 0);
+  return sum === n ? n : thirt(sum);
+}
