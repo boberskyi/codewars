b819fedafcddeebc86ee92163f227821faf4b411
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-07-27 12:35:44 +0200

    Create Multiplication table.js

---
+++ Multiplication table.js
@@ -0,0 +1,15 @@
+const multiplicationTable = size => {
+  const table = [];
+
+  for (let i = 1; i <= size; i++) {
+    const row = [];
+
+    for (let j = 1; j <= size; j++) {
+      row.push(i * j);
+    }
+
+    table.push(row);
+  }
+
+  return table;
+}
