8fa1f9846fbd27922a8316f20c21ac2118fc3605
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-08-10 13:26:21 +0200

    Create Directions Reduction.js

---
+++ Directions Reduction.js
@@ -0,0 +1,5 @@
+const dirReduc = arr => {
+  let str = arr.join(''), pattern = /NORTHSOUTH|EASTWEST|SOUTHNORTH|WESTEAST/;
+  while (pattern.test(str)) str = str.replace(pattern, '');
+  return str.match(/(NORTH|SOUTH|EAST|WEST)/g) || [];
+}
