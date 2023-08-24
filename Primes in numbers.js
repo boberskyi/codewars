e06961c29f5b829ca0965bfa589b5279cc6c4e26
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-08-24 14:45:51 +0200

    Create Primes in numbers.js

---
+++ Primes in numbers.js
@@ -0,0 +1,8 @@
+const primeFactors = n => {
+  for (var i = 2, res = "", f; i <= n; i++) {
+    f = 0;
+    while (n % i == 0) { f++; n /= i }
+    res += f ? "(" + (f > 1 ? i + "**" + f : i) + ")" : ""
+  }
+  return res || "(" + n + ")"
+}
