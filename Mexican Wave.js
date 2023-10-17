a2f81b4a23aa8c9b32767ed7d145757ad4eb7b1d
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-10-17 08:31:25 +0200

    Create Mexican Wave.js

---
+++ Mexican Wave.js
@@ -0,0 +1,12 @@
+const wave = str => {
+  const result = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      const waveStr = str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1);
+      result.push(waveStr);
+    }
+  }
+
+  return result;
+}
